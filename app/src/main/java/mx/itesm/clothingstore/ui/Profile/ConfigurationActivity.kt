78000cba997c79cef9dff67f1dc83d47712536db
Maribel Melendez <lizbethmelendez@live.com>
2021-10-11 22:26:20 -0500

    Crear fragmentos de configuración

--- app/src/main/java/mx/itesm/clothingstore/ui/Profile/ConfigurationActivity.kt
+++ app/src/main/java/mx/itesm/clothingstore/ui/Profile/ConfigurationActivity.kt
@@ -2,6 +2,7 @@ package mx.itesm.clothingstore.ui.Profile
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
 import mx.itesm.clothingstore.R
 import mx.itesm.clothingstore.databinding.ActivityConfigurationBinding
@@ -16,6 +17,44 @@ class ConfigurationActivity : AppCompatActivity() {
         binding = ActivityConfigurationBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        val extras = intent.extras
+        var type = ""
+        if (extras != null) {
+            type = extras.getString("type").toString()
+            when (type) {
+                "address" -> {replaceAddressFragment()}
+                "payment" -> {replacePaymentFragment()}
+                else -> {replaceDataFragment()}
+            }
+        } else {
+            Toast.makeText(this, "No se pudo obtener la información", Toast.LENGTH_SHORT).show()
+        }
+    }
 
+
+
+    private fun replaceDataFragment() {
+        val fragment = ConfigDataFragment()
+        val transact = supportFragmentManager.beginTransaction()
+        transact.add(R.id.configFcContainer, fragment, TAG_FRAGMENT)
+        transact.commit()
+    }
+
+    private fun replaceAddressFragment() {
+        val fragment = ConfigAddressFragment()
+        val transact = supportFragmentManager.beginTransaction()
+        transact.add(R.id.configFcContainer, fragment, TAG_FRAGMENT)
+        transact.commit()
+    }
+
+    private fun replacePaymentFragment() {
+        val fragment = ConfigPaymentFragment()
+        val transact = supportFragmentManager.beginTransaction()
+        transact.add(R.id.configFcContainer, fragment, TAG_FRAGMENT)
+        transact.commit()
+    }
+
+    companion object{
+        private const val TAG_FRAGMENT= "config_fragment"
     }
 }
